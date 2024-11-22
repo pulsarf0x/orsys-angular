d284cd4480d0c0ce8ec69efdb210110cdc5b5e40
pulsarf0x <pascal@plumia.net>
2024-11-22 13:28:06 +0100

    Add message service

--- src/app/core/interfaces/crud-service.ts
+++ src/app/core/interfaces/crud-service.ts
@@ -1,2 +1,23 @@
-export interface CrudService {
+import { Observable } from "rxjs";
+
+export interface UniqueData {
+    id: number | string
+}
+
+export interface MethodAcceptance {
+    label: 'ACCEPTED' | 'REJECTED'
+    reason: string
+}
+
+export interface CrudService<D extends UniqueData> {
+    data$: Observable<D[]>
+
+    create(data: Omit<D, 'id'>): Observable<MethodAcceptance>
+
+    read(): Observable<MethodAcceptance>
+
+    update(target: D, data: Partial<D>): Observable<MethodAcceptance>
+
+    delete(): Observable<MethodAcceptance>
+
 }
--- src/app/features/message/services/message.service.ts
+++ src/app/features/message/services/message.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { CrudService, MethodAcceptance, UniqueData } from '../../../core/interfaces/crud-service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+export interface Message extends UniqueData {
+  text: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class MessageService {
+export class MessageService implements CrudService<Message> {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private _data = new BehaviorSubject<Message[]>([])
+  data$ = this._data.asObservable();
+
+  create(data: Omit<Message, 'id'>): Observable<MethodAcceptance> {
+    throw new Error('Method not implemented.');
+  }
+  
+  read(): Observable<MethodAcceptance> {
+    throw new Error('Method not implemented.');
+  }
+
+  update(target: Message, data: Partial<Message>): Observable<MethodAcceptance> {
+    throw new Error('Method not implemented.');
+  }
+
+  delete(): Observable<MethodAcceptance> {
+    throw new Error('Method not implemented.');
+  }
 }
